d0abc4b3a279901fa4892e898e657ff663f3adba
MrSanx <sanchezestudios3111@gmail.com>
2020-10-04 18:19:50 -0500

    Initial commit

---
+++ src/main/webapp/index.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+   var rand = Math.floor(Math.random()*100);
+   document.getElementById("aleatorio").innerText = "Numero aleatorio: "+rand;
+   
+   var ajax= new XMLHttpRequest();
+   ajax.onreadystatechange = new function() {
+    if (this.readystate==4 && this.status==200) {
+            document.getElementById("mensaje").innerText = this.responseText;
+        }
+    }
+   ajax.open("GET", "./Hello",true);//Esto es un cambio cualquiera
+   ajax.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+  ajax.send();
+}
+
+function actualizarMensaje(){
+    alert("cambiaste el estudiante");
+}
