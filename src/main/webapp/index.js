865abcfe1193b6cbd76977599d411874075b469f
MrSanx <sanchezestudios3111@gmail.com>
2020-10-04 22:15:59 -0500

    Actualizar Mensaje

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,18 +1,33 @@
-window.onload = function() {
-   var rand = Math.floor(Math.random()*100);
-   document.getElementById("aleatorio").innerText = "Numero aleatorio: "+rand;
-   
-   var ajax= new XMLHttpRequest();
-   ajax.onreadystatechange = new function() {
-    if (this.readystate==4 && this.status==200) {
+window.onload = function () {
+    var rand = Math.floor(Math.random() * 100);
+    document.getElementById("aleatorio").innerText = "Numero aleatorio: " + rand;
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = new function () {
+        if (this.readystate == 4 && this.status == 200) {
             document.getElementById("mensaje").innerText = this.responseText;
         }
     }
-   ajax.open("GET", "./Hello",true);//Esto es un cambio cualquiera
-   ajax.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-  ajax.send();
+    ajax.open("GET", "./Hello", true);//Esto es un cambio cualquiera
+    ajax.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    ajax.send();
 }
 
-function actualizarMensaje(){
-    alert("Cambiaste el estudiante");
+function actualizarMensaje() {
+    //Alerta de cambio
+    alert("Cambiaste de miembro.");
+    //Variable de comodidad
+    var x = document.getElementById("selected");
+    var i = x.selectedIndex;
+    //Variables de mensaje
+    var nombre = x.options[i].text;
+    var id = x.options.selectedIndex + 1;
+    var categoria = "";
+    if (id == 1) {
+        categoria = "profesor"
+    } else {
+        categoria = "estudiante"
+    }
+    //Mensaje
+    document.getElementById("mensaje").innerText = ("Bienvenido " + categoria + " " + nombre + " con ID 0" + id + " , esperemos que tenga usted un buen día.");
 }
